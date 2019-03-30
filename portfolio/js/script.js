0920ea1cc4f0d77101efed7f428ca29f062ba129
Hawari siregar <48271381+Hawarisiregar1814370608@users.noreply.github.com>
2019-03-30 18:49:39 +0700

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,14 @@
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+$('.page-scroll').on('click', function(e) {
+
+ var tujuan = $(this).attr('href');
+
+ var elemenTujuan = $(tujuan);
+
+ $('html , body').animate({
+  scrollTop: elemenTujuan.offset().top - 50
+ });
+
+ e.preventDefault();
+});﻿
